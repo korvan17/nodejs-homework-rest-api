2a1f5bbffd81bbd9859a0282998ae230f6e78df9
korvan17 <ivan@homeds.pro>
2023-07-15 22:49:03 +0300

    fix error mess

--- middlewars/isEmptyBody.js
+++ middlewars/isEmptyBody.js
@@ -3,7 +3,7 @@ import { HttpError } from "../helpers/index.js";
 const isEmptyBody = (req, res, next) => {
   const { length } = Object.keys(req.body);
   if (!length) {
-    next(HttpError(400, "Body need requied fields"));
+    next(HttpError(400, "missing fields"));
   }
   next();
 };
--- schemas/contacts-schemas.js
+++ schemas/contacts-schemas.js
@@ -2,13 +2,13 @@ import Joi from "joi";
 
 const contactsAddSchema = Joi.object({
   name: Joi.string().required().messages({
-    "any.required": `"name" must be exist`,
+    "any.required": `missing required name field`,
   }),
   email: Joi.string().required().messages({
-    "any.required": `"email" must be exist`,
+    "any.required": `missing required email field`,
   }),
   phone: Joi.string().required().messages({
-    "any.required": `"phone" must be exist`,
+    "any.required": `missing required phone field`,
   }),
 });
 
