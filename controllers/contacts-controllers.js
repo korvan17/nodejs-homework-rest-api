cc3baab61b1230440c064f860afb2d83511c21f7
korvan17 <ivan@homeds.pro>
2023-08-07 21:02:47 +0300

    fix reg to req

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -8,7 +8,7 @@ const messageNotFound = "`Not found`";
 
 async function getAll(req, res) {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = reg.query;
+  const { page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
   const result = await Contact.find({ owner }, "", { skip, limit }).populate(
     "owner",
