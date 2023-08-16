84e4b8327293177f99b2e855e4bfba1686a65c56
korvan17 <ivan@homeds.pro>
2023-08-16 17:35:30 +0300

    fix upload avatar

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -94,7 +94,11 @@ async function updateAvatar(req, res) {
   await User.findByIdAndUpdate(_id, { avatarURL });
 
   res.json({ avatarURL });
-  fs.unlink(oldPath);
+  try {
+    fs.unlink(oldPath);
+  } catch (error) {
+    console.log("error remove file - ", error);
+  }
 }
 
 export default {
